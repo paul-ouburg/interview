2d0be77ed77ccc5877126f5e53efc664d3caee6d
Paul Ouburg <paul.ouburg@gmail.com>
2025-01-08 10:14:12 +0100

    mockito test

--- src/test/java/org/example/mockito/CalculatorServiceTest.java
+++ src/test/java/org/example/mockito/CalculatorServiceTest.java
@@ -3,16 +3,15 @@ package org.example.mockito;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class CalculatorServiceTest {
 
     @Test
-    void add() {
+    void addTest() {
         // 1) create the mock object of CalulatorService
 
-        // 2) mock the behaviour of the .add method in CalculatorService for 2 + 3
+        // 2) mock the behaviour of the .add method in CalculatorService for values 2 + 3
 
         // 3) assert that 2 + 3 = 5
     }
